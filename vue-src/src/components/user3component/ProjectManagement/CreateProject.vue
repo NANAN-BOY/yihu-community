1f5bd36c975601c88aacf8648e38cb5aa4c569b7
NANAN <nanansole@outlook.com>
2025-01-21 15:10:17 +0800

    修复：修复用户申报项目必填的bug

--- vue-src/src/components/user3component/ProjectManagement/CreateProject.vue
+++ vue-src/src/components/user3component/ProjectManagement/CreateProject.vue
@@ -3,21 +3,47 @@
     <br/>
     <!-- 创建表单 -->
     <el-form :model="formData" ref="formRef" label-width="120px">
-      <el-form-item label="项目名称" prop="project_name">
-  <el-input v-model="formData.project_name" placeholder="请输入项目名称" />
-</el-form-item>
+      <el-form-item label="项目名称*" prop="project_name">
+        <el-input v-model="formData.project_name" placeholder="请输入项目名称" />
+      </el-form-item>
 
-      <el-form-item v-for="(field, index) in templateData.fields" :key="index" :label="field.templateFields_name" :prop="'field_' + index">
+      <el-form-item
+          v-for="(field, index) in templateData.fields"
+          :key="index"
+          :label="getLabel(field.templateFields_name, field.templateFields_isRequired)"
+          :prop="'field_' + index"
+      >
         <!-- 根据字段类型动态渲染表单控件 -->
-        <el-input v-if="field.templateFields_type === 1" v-model="formData['field_' + index].value" :placeholder="`请输入${field.templateFields_name}`" :disabled="!field.templateFields_isRequired" />
-        
-        <el-input v-if="field.templateFields_type === 2" type="textarea" v-model="formData['field_' + index].value" :placeholder="`请输入${field.templateFields_name}`" :disabled="!field.templateFields_isRequired" />
-        
-        <el-select v-if="field.templateFields_type === 3" v-model="formData['field_' + index].value" placeholder="请选择" :disabled="!field.templateFields_isRequired">
+        <el-input
+            v-if="field.templateFields_type === 1"
+            v-model="formData['field_' + index].value"
+            :placeholder="`请输入${field.templateFields_name}`"
+            :required="field.templateFields_isRequired"
+        />
+
+        <el-input
+            v-if="field.templateFields_type === 2"
+            type="textarea"
+            v-model="formData['field_' + index].value"
+            :placeholder="`请输入${field.templateFields_name}`"
+            :required="field.templateFields_isRequired"
+        />
+
+        <el-select
+            v-if="field.templateFields_type === 3"
+            v-model="formData['field_' + index].value"
+            placeholder="请选择"
+            :required="field.templateFields_isRequired"
+        >
           <el-option v-for="(option, idx) in JSON.parse(field.templateFields_options)" :key="idx" :label="option" :value="option" />
         </el-select>
 
-        <el-upload v-if="field.templateFields_type === 4" :action="uploadUrl" :disabled="!field.templateFields_isRequired" :on-success="handleUploadSuccess" :before-upload="beforeUpload">
+        <el-upload
+            v-if="field.templateFields_type === 4"
+            :action="uploadUrl"
+            :on-success="handleUploadSuccess"
+            :before-upload="beforeUpload"
+        >
           <el-button size="small" type="primary">点击上传</el-button>
         </el-upload>
       </el-form-item>
@@ -29,23 +55,24 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted,defineProps, defineEmits } from 'vue';
+import { ref, onMounted, defineProps, defineEmits } from 'vue';
 import axios from 'axios';
 import { ElForm, ElInput, ElSelect, ElOption, ElButton, ElUpload, ElMessageBox, ElMessage } from 'element-plus';
 import store from '../../../store';
-const templateData = ref<any>({});
-  const formData = ref<any>({ project_name: '' });
 
+const templateData = ref<any>({});
+const formData = ref<any>({ project_name: '' });
 const uploadUrl = ref(`${import.meta.env.VITE_BACKEND_IP}/api/upload`); // 上传文件的 URL
 const emit = defineEmits<{
   (e: 'closeForm', formData: any): void;
 }>();
+
 // 获取启用的模板数据
 const fetchTemplateData = async () => {
   try {
     const response = await axios.get(`${import.meta.env.VITE_BACKEND_IP}/api/template/getEnableProjectTemplate`);
     templateData.value = response.data;
-    
+
     // 1. 初始化 `formData` 中的项目名称
     formData.value.project_name = '';  // 确保项目名称字段存在并初始化
 
@@ -61,13 +88,17 @@ const fetchTemplateData = async () => {
   }
 };
 
+// 获取标签内容（带 * 的必填项）
+const getLabel = (label: string, isRequired: boolean) => {
+  return isRequired ? `${label} *` : label;
+};
 
 // 表单提交
 const submitForm = async () => {
   ElMessageBox.confirm(
-    '你确定要提交表单吗？',
-    '确认提交',
-    { confirmButtonText: '确定', cancelButtonText: '取消', type: 'warning' }
+      '你确定要提交表单吗？',
+      '确认提交',
+      { confirmButtonText: '确定', cancelButtonText: '取消', type: 'warning' }
   ).then(async () => {
     // 校验必填项
     const invalidFields = templateData.value.fields.filter((field: any, index: number) => {
@@ -109,7 +140,6 @@ const submitForm = async () => {
     ElMessage.info('你取消了提交');
   });
 };
-
 
 // 上传文件成功后的回调
 const handleUploadSuccess = (response: any, file: any, fileList: any) => {

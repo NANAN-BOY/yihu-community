b8b1924673dff5dcb1a4771d6e131b65959f35bc
NANAN <nanansole@outlook.com>
2025-02-24 17:50:41 +0800

    移除：移除无用内容

--- routes/indexRouter.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '智能管理平台' });
-});
-
-module.exports = router;

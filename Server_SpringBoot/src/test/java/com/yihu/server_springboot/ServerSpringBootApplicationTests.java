be4c0481022f2ed0c89c538a4dadc55218190756
NANAN <nanansole@outlook.com>
2025-02-26 19:48:36 +0800

    构建：构建SpringBoot服务端

---
+++ Server_SpringBoot/src/test/java/com/yihu/server_springboot/ServerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yihu.server_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

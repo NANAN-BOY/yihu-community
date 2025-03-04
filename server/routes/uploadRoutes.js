f5aba5bd75db49c224dcfd7444f87792c37738af
NANAN <nanansole@outlook.com>
2025-03-04 15:32:03 +0800

    清理：移除旧版2.0版本后端代码

--- server/routes/uploadRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const uploadController = require('../controllers/uploadController');
-const authenticateToken = require('../middlewares/authMiddleware');
-
-router.post('/upload', authenticateToken, uploadController.uploadFile);
-
-module.exports = router;
--- server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-// 定义路由
-router.get('/users/list', userController.getUserListByRoleAndStatus);
-router.get('/users/getUserInfo/:id', userController.getUserById);
-
-module.exports = router;

d54d6a2a70472cc44a52b23eeca151cbfde8e259
NANAN <nanansole@outlook.com>
2025-03-24 19:59:53 +0800

    更新：前端更新专家定制订单列表

--- vue-src/src/components/user3component/ExpertCustomization/ExpertCustomization.vue
+++ vue-src/src/components/user3component/ExpertCustomization/ExpertCustomization.vue
@@ -1,5 +1,48 @@
 <script setup>
+import {computed, ref} from "vue";
+import axios from "axios";
+import store from "../../../store";
 
+const OrderList = ref([])
+const currentPage = ref(1)
+const loading = ref(false)
+const error = ref('')
+const hasMore = ref(true)
+const noMore = computed(() => !hasMore.value)
+const disabled = computed(() => loading.value || noMore.value)
+const OrderListLoad = async () => {
+  if (disabled.value) return
+  try {
+    loading.value = true
+    error.value = ''
+    const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_IP}/api/user/get-myOrderList`,
+        {
+          type: null,
+          status: null,
+        },
+        {
+          params: {
+            pageNum: currentPage.value,
+            pageSize: 10
+          },
+          headers: {
+            token: store.state.token
+          }
+        }
+    )
+    console.log(response.data)
+    if (response.data.code === 200) {
+      OrderList.value = [...OrderList.value, ...response.data.data.list]
+      hasMore.value = response.data.data.hasNextPage
+      currentPage.value++
+    }
+  } catch (err) {
+    error.value = '数据加载失败，请稍后再试'
+  } finally {
+    loading.value = false
+  }
+}
 </script>
 
 <template>
@@ -8,8 +51,49 @@
   </el-breadcrumb>
   <h1>定制服务</h1>
   <el-button type="primary" @click=""> 创建定制服务</el-button>
+  <!-- 我的订单无限滚动列表 -->
+  <div class="infinite-list-wrapper" style="overflow: auto">
+    <ul
+        v-infinite-scroll="OrderListLoad"
+        class="list"
+        :infinite-scroll-disabled="disabled"
+    >
+      <li v-for="Order in OrderList" :key="Order.id" class="list-item" @click="">
+        <div>{{ Order.createAt }}</div>
+      </li>
+      <li v-if="loading" v-loading="loading" class="list-item"></li>
+    </ul>
+    <p v-if="loading">加载中...</p>
+    <p v-if="noMore">没有更多数据了</p>
+    <p v-if="error" style="color: red">{{ error }}</p>
+  </div>
 </template>
 
 <style scoped>
+.infinite-list-wrapper {
+  height: 80vh;
+  text-align: center;
+}
 
+.infinite-list-wrapper .list {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+
+.infinite-list-wrapper .list-item:hover {
+  background: #b6b6b6;
+  transition: background 0.3s ease;
+  cursor: pointer;
+}
+
+.infinite-list-wrapper .list-item {
+  border-radius: 10px;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  //justify-content: center; height: 50px;
+  background: #f5f5f5;
+  color: #000000;
+}
 </style>

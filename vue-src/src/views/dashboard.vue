b05b721430a9f49a5818338c2b145ea99c1ab258
NANAN <nanansole@outlook.com>
2025-06-15 16:09:00 +0800

    更新：前端为每个角色新增首页功能（页面暂未设计）

--- vue-src/src/views/dashboard.vue
+++ vue-src/src/views/dashboard.vue
@@ -251,7 +251,7 @@ import aboutMyInfo from '../components/user/aboutMyInfo/aboutMyInfo.vue';
 import Communicate from '../components/user/Communicate.vue'
 
 
-import {Avatar, Document, Expand, Star, StarFilled, Tickets, UserFilled} from "@element-plus/icons-vue";
+import {Avatar, Document, Expand, HomeFilled, Star, StarFilled, Tickets, UserFilled} from "@element-plus/icons-vue";
 import {ElMessage, ElMessageBox} from "element-plus";
 import axios from "axios";
 
@@ -282,34 +282,38 @@ onMounted(() =>{
 
 // MENUConfig
 const menus = [
-  {role: 1, index: '1-1', icon: Document, title: '审核', children: [
-      {index: '1-1-1', title: '项目申报审核'},
-      {index: '1-1-2', title: '活动审核'}
+  {role: 1, index: '1-1', icon: HomeFilled, title: '首页'},
+  {role: 1, index: '1-2', icon: Document, title: '审核', children: [
+      {index: '1-2-1', title: '项目申报审核'},
+      {index: '1-2-2', title: '活动审核'}
     ]
   },
-  {role: 1, index: '1-2', icon: Document, title: '专家库管理'},
-  {role: 1, index: '1-3', icon: Avatar, title: '系统管理'},
-  {role: 1, index: '1-4', icon: Tickets, title: '满意度调查模板管理'},
-  {role: 1, index: '1-5', icon: Document, title: '关于我的信息'},
+  {role: 1, index: '1-3', icon: Document, title: '专家库管理'},
+  {role: 1, index: '1-4', icon: Avatar, title: '系统管理'},
+  {role: 1, index: '1-5', icon: Tickets, title: '满意度调查模板管理'},
+  {role: 1, index: '1-6', icon: Document, title: '关于我的信息'},
 
-  {role: 2, index: '2-1', icon: Document, title: '项目管理'},
-  {role: 2, index: '2-2', icon: Avatar, title: '专家库管理'},
+  {role: 2, index: '2-1', icon: HomeFilled, title: '首页'},
+  {role: 2, index: '2-2', icon: Document, title: '项目管理'},
+  {role: 2, index: '2-3', icon: Avatar, title: '专家库管理'},
   {
-    role: 2, index: '2-3', icon: Tickets, title: '模板管理', children: [
-      {index: '2-3-1', title: '项目申报模板'},
-      {index: '2-3-2', title: '活动细节模板'}
+    role: 2, index: '2-4', icon: Tickets, title: '模板管理', children: [
+      {index: '2-4-1', title: '项目申报模板'},
+      {index: '2-4-2', title: '活动细节模板'}
     ]
   },
-  {role: 2, index: '2-4', icon: UserFilled, title: '社会组织管理'},
-  {role: 2, index: '2-5', icon: Document, title: '关于我的信息'},
+  {role: 2, index: '2-5', icon: UserFilled, title: '社会组织管理'},
+  {role: 2, index: '2-6', icon: Document, title: '关于我的信息'},
 
-  {role: 3, index: '3-1', icon: Document, title: '项目管理'},
-  {role: 3, index: '3-2', icon: StarFilled, title: '专家定制'},
-  {role: 3, index: '3-3', icon: Document, title: '关于我的信息'},
+  {role: 3, index: '3-1', icon: HomeFilled, title: '首页'},
+  {role: 3, index: '3-2', icon: Document, title: '项目管理'},
+  {role: 3, index: '3-3', icon: StarFilled, title: '专家定制'},
+  {role: 3, index: '3-4', icon: Document, title: '关于我的信息'},
 
-  {role: 4, index: '4-1', icon: Document, title: '接单广场'},
-  {role: 4, index: '4-2', icon: Document, title: '我的订单'},
-  {role: 4, index: '4-3', icon: Document, title: '关于我的信息'}
+  {role: 4, index: '4-1', icon: HomeFilled, title: '首页'},
+  {role: 4, index: '4-2', icon: Document, title: '接单广场'},
+  {role: 4, index: '4-3', icon: Document, title: '我的订单'},
+  {role: 4, index: '4-4', icon: Document, title: '关于我的信息'}
 ];
 
 // HandleMENUSelect
@@ -321,59 +325,73 @@ const handleSelect = (index) => {
   }
 
   switch (index) {
-    case '1-1-1':
+    case '1-1':
+      currentComponent.value = null;
+      break;
+    case '1-2-1':
       currentComponent.value = ProjectExamine;
       break;
-    case '1-1-2':
+    case '1-2-2':
       currentComponent.value = ActivityExamine;
       break;
-    case '1-2':
+    case '1-3':
       currentComponent.value = user1ExpertLibraryManagement;
       break;
-    case '1-3':
+    case '1-4':
       currentComponent.value = SystemSetting;
       break;
-    case '1-4':
+    case '1-5':
       currentComponent.value = ActivityQuestionnaireSetting;
       break;
-    case '1-5':
+    case '1-6':
       currentComponent.value = aboutMyInfo;
       break;
 
     case '2-1':
-      currentComponent.value = user2ProjectManagement;
+      currentComponent.value = null;
       break;
     case '2-2':
+      currentComponent.value = user2ProjectManagement;
+      break;
+    case '2-3':
       currentComponent.value = user1ExpertLibraryManagement;
       break;
-    case '2-3-1':
+    case '2-4-1':
       currentComponent.value = user2ProjectTemplateManagement;
       break;
-    case '2-3-2':
+    case '2-4-2':
       currentComponent.value = user2ActivityTemplateManagement;
       break;
-    case '2-4':
+    case '2-5':
       currentComponent.value = user2SocialOrganizationManagement;
       break;
-    case '2-5':
+    case '2-6':
       currentComponent.value = aboutMyInfo;
       break;
+
     case '3-1':
-      currentComponent.value = ProjectManagement;
+      currentComponent.value = null;
       break;
     case '3-2':
-      currentComponent.value = ExpertCustomization;
+      currentComponent.value = ProjectManagement;
       break;
     case '3-3':
+      currentComponent.value = ExpertCustomization;
+      break;
+    case '3-4':
       currentComponent.value = aboutMyInfo;
       break;
+
     case '4-1':
-      currentComponent.value = ReceiveOrder;
+      currentComponent.value = null;
       break;
     case '4-2':
-      currentComponent.value = MyReceiveOrder;
+      currentComponent.value = ReceiveOrder;
       break;
     case '4-3':
+      currentComponent.value = MyReceiveOrder;
+      break;
+    case '4-4':
       currentComponent.value = aboutMyInfo;
       break;
     default:
